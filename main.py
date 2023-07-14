6e060e21e362e99b3263f478768e49ebbd1fe6d6
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-07-14 17:10:45 +0330

    CHNAGE & REFACTOR : i'm changed and deleted some code with module file

--- main.py
+++ main.py
@@ -3,37 +3,8 @@ from termcolor import colored
 from ascii_module.choice_ascii_string import get_random_symbols, get_random_numbers, get_random_lower_case, \
     get_random_upper_case
 from settings_password_module.settings_password import print_password_generator_text, clear_screen, \
-    yes_or_no, get_length_from_user, settings
-
-def get_settings_from_user(settings):
-    for option, default in settings.items():
-        if option != "length":
-            user_choice = yes_or_no(option, default)
-
-            settings[option] = user_choice
-
-        else:
-            user_length = get_length_from_user(option, default)
-
-            settings[option] = user_length
-
-
-def ask_if_change_settings(settings):
-    while True:
-
-        user_answer = input("do you want change default settings?"
-                            "(y: yes, n: no, enter: yes): ")
-
-        if user_answer in ['y', 'n', '']:
-            if user_answer in ['y', '']:
-                print('-' * 5, colored('Change Settings', 'blue'), '-' * 5, sep='')
-                get_settings_from_user(settings)
-            break
-
-        else:
-            print(colored("Invalid input.", 'red'))
-            print(colored("Please try again.", 'red'))
-
+    settings
+from settings_password_module.ask_settings_from_user import get_settings_from_user, ask_if_change_settings
 
 def generate_random_char(choices):
     choice = random.choice(choices)
